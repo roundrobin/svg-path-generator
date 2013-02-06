459acac957d45613fd5126b294d9ed07aed6ae6d
Nils <nils.schlomann@gmail.com>
2013-02-05 21:50:54 -0800

    Adds data to the circle handler

--- svg-path-generator.js
+++ svg-path-generator.js
@@ -52,8 +52,8 @@ Path.prototype.render  = function(){
     }
 
     if(op === 'c'){
-      cx =  cx + el[5];
-      cy =  cy + el[6];
+    cx =  cx + el[5];
+    cy =  cy + el[6];
     }
 
     if(op === 'v'){
@@ -61,42 +61,45 @@ Path.prototype.render  = function(){
     }
 
     if(op === 'h'){
-      cx =  cx + el[1];
+    cx =  cx + el[1];
     }
 
 
     if(op === 'A'){
-      cx = el[6];
-      cy = el[7];
+    cx = el[6];
+    cy = el[7];
     }
 
     if(op === 'a'){
-      cx = cx + el[6];
-      cy = cy + el[7];
+    cx = cx + el[6];
+    cy = cy + el[7];
     }
 
 
     if(op === 'C'){
-      cx = el[5];
-      cy = el[6];
+    cx = el[5];
+    cy = el[6];
     }
     if(op === 'Q' || op === 'S'){
-      cx = el[3];
-      cy = el[4];
+    cx = el[3];
+    cy = el[4];
 
     }
     if(op === 'H'){
-      cx = el[1];
+    cx = el[1];
+
     }
     if(op === 'V'){
-      cy = el[1];
+    cy = el[1];
 
     }    
-    var dragPoint = d3.behavior.drag()
-      .on("drag", function(){ console.log('Dragging')})
-      .on("dragstart", function(d, i) {})
-      .on("dragend", function(d, i) {});
 
+    var drag = d3.behavior.drag()
+    .on("drag", function(d,i){console.log('draggin',d,i,d3.select(this).data())})
+    .on("dragstart", function(d, i) {})
+    .on("dragend", function(d, i) {});
+    
+    
     this.canvas.append("circle")
     .attr({
       r: 5,
@@ -106,8 +109,8 @@ Path.prototype.render  = function(){
       class: "pointers",
       opacity: "0"
     })
-    .data({type: el, nr: i})
-    .call(drag);
+    .call(drag)
+    .datum({type: el, index: i});
     
   }
   this.path.attr("d",this.pathWay());
